aff63870c73551af27356333aca76abcc02f7bd8
MinexAutomation <minex.automation@gmail.com>
2019-05-22 16:54:38 -0400

    Added FileStreamHelper.

---
+++ source/R5T.NetStandard.IO.Types/Code/Classes/FileStreamHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+
+namespace R5T.NetStandard.IO
+{
+    public static class FileStreamHelper
+    {
+        /// <summary>
+        /// Eases construction of a new <see cref="FileStream"/> with a best-practice implementation of handling the overwrite parameter.
+        /// </summary>
+        public static FileStream NewWrite(string filePath, bool overwrite = true)
+        {
+            FileMode fileMode = FileMode.Create;
+            if (!overwrite)
+            {
+                fileMode = FileMode.CreateNew;
+            }
+
+            var fileStream = new FileStream(filePath, fileMode);
+            return fileStream;
+        }
+
+        /// <summary>
+        /// Eases construction of a new <see cref="FileStream"/> for reading.
+        /// </summary>
+        public static FileStream NewRead(string filePath)
+        {
+            var fileStream = new FileStream(filePath, FileMode.Open);
+            return fileStream;
+        }
+    }
+}
