3801d153a780145c603efd63b26bba548264a118
MinexAutomation <minex.automation@gmail.com>
2019-06-04 12:32:28 -0400

    Added SearchPattern type. Moved helper code files.

---
+++ source/R5T.NetStandard.IO.Types/Code/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace R5T.NetStandard.IO.Extensions
+{
+    public static class StringExtensions
+    {
+        public static SearchPattern AsSearchPattern(this string value)
+        {
+            var searchPattern = new SearchPattern(value);
+            return searchPattern;
+        }
+    }
+}
