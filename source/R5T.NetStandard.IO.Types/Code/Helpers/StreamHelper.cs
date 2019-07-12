f4fa49078593f7e5f6da6b7e511240c24f5a0bfc
MinexAutomation <minex.automation@gmail.com>
2019-07-12 00:03:07 -0400

    Added string to stream helper method.

---
+++ source/R5T.NetStandard.IO.Types/Code/Helpers/StreamHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace R5T.NetStandard.IO
+{
+    public static class StreamHelper
+    {
+        public static Stream FromString(string value)
+        {
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(value));
+            return stream;
+        }
+    }
+}
