bab5a1a990de1b0607586139072bd6daae66935e
MinexAutomation <minex.automation@gmail.com>
2019-08-22 16:01:32 -0400

    Changed name to reflect broadening of functionality from StreamReader to any TextReader.

---
+++ source/R5T.NetStandard.IO.Types/Code/Helpers/TextReaderHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+
+namespace R5T.NetStandard.IO
+{
+    public static class TextReaderHelper
+    {
+        public const string EndOfString = null;
+
+
+        public static bool ReadLine(this TextReader textReader, out string line)
+        {
+            line = textReader.ReadLine();
+
+            var isEnd = line == TextReaderHelper.EndOfString;
+            return isEnd;
+        }
+    }
+}
