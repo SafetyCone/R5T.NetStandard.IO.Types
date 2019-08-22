be08e117e0eb2997106364d2c04b07237c67cbba
MinexAutomation <minex.automation@gmail.com>
2019-08-22 16:03:47 -0400

    Follow-through on name change to TextReaderHelper from StringReaderExtension.

--- source/R5T.NetStandard.IO.Types/Code/Extensions/StringReaderExtensions.cs
+++ source/R5T.NetStandard.IO.Types/Code/Extensions/StringReaderExtensions.cs
@@ -8,7 +8,7 @@ namespace R5T.NetStandard.IO
     {
         public static bool ReadLineIsEnd(this StringReader stringReader, out string line)
         {
-            var output = StringReaderHelper.ReadLine(stringReader, out line);
+            var output = TextReaderHelper.ReadLine(stringReader, out line);
             return output;
         }
     }
